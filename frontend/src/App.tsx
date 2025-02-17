b9dbfa4abd220933eedbb378d4d1aeab8ebea4ad
Ruan Emanuell Alencar <ruanvitorcom540@gmail.com>
2025-02-17 09:33:36 -0300

    feat: starting project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import logo from './assets/logo.png'
+
+function App() {
+  return (
+    <div className='bg-[#FEBF08] w-screen h-screen flex flex-col justify-center items-center'>
+      <img src={logo}></img>
+      <h1 className='text-4xl font-bold animate-pulse'>Em construção...</h1>
+    </div>
+  )
+}
+
+export default App
