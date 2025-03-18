e6b6330e829a0b13a09747ccd03491af4f6b588a
Gabriel <gabrielmarcaldev1@gmail.com>
2025-03-18 10:52:43 -0300

    Login e Logout

--- backend/src/controllers/usuarios.js
+++ backend/src/controllers/usuarios.js
@@ -51,8 +51,66 @@ async function criaUsuario(req, res) {
     }
 }
 
+async function loginUsuario(req, res){
+    const { email, senha } = req.body
+    try {
+        const usuario = await prisma.usuario.findUnique({
+            where : {email : email},
+        });
+        console.log("Usuario encontrado : ", usuario);
+
+        if(!usuario){
+            return res.status(401).json({message : "Credenciais invalidas"})
+        }
+
+        const senhaCorreta = await bcrypt.compare(senha, usuario.senha)
+        console.log("Senha correta : ", senhaCorreta)
+
+        if(!senhaCorreta){
+            return res.status(401).json({message : "Credenciais invalidas"})
+        }
+
+        if(!process.env.SECRET_JWT){
+            console.error("SECRET_JWT nao definido!");
+            return res
+                .status(500)
+                .json({ message : "Erro no servidor : SECRET_JWT nao definido"})
+        }
+
+        const token = jwt.sign(
+            {id : usuario.id, email : usuario.email},
+            process.env.SECRET_JWT,
+            {
+                expiresIn : "1h",
+            }
+        );
+        res.status(200).json({message : "Login bem-sucedido!", token})
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message : "Erro ao autenticar usuario."});
+    }
+}
+
+async function logout(req, res){
+    const token =
+        req.headers.authorization && req.headers.authorization.split(" ")[1];
+        try {
+            if(token){
+                blackListToken(token);
+                res.status(200).json({message : "Logout realizado com sucesso"})
+            }
+            else{
+                res.status(400).json({ Message : "Token nao fornecido"})
+            }
+        } catch (error) {
+            console.log(error)
+            res.status(500).json({ Message : "Erro ao deslogar"})
+        }
+}
 
 module.exports = {
     criaUsuario,
+    loginUsuario,
+    logout,
 };
 
--- backend/src/routes/usuarios.js
+++ backend/src/routes/usuarios.js
@@ -5,5 +5,7 @@ const usuarioController = require('../controllers/usuarios')
 const token = require('../authMiddleware')
 
 router.post('/',usuarioController.criaUsuario);
+router.post('/login', usuarioController.loginUsuario)
+router.post('/logout', usuarioController.logout)
 
 module.exports = router
