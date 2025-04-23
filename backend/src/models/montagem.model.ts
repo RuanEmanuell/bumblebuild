1fffbed0e89c666fc242e882f867e8e8384cbd80
Aliny <aliny1505dutra@gmail.com>
2025-04-23 15:58:00 -0300

    feat: cria model Montagem e atualiza schema Prisma

---
+++ backend/src/models/montagem.model.ts
@@ -0,0 +1,14 @@
+export interface Montagem {
+    id: number;
+    usuarioId: number;
+    cpuId: number;
+    gpuId: number;
+    placaMaeId: number;
+    ramId: number;
+    fonteId: number;
+    ssdId: number;
+    gabineteId: number;
+    quantidadeRAM: number;
+    quantidadeSSD: number;
+    dataMontagem: Date; 
+}
