676f54e4e38f4989f735ae11cd8f5cd785a4d91f
Ruan Emanuell Alencar <ruanvitorcom540@gmail.com>
2025-07-09 21:16:36 -0300

    feat: changing update hour

--- backend/src/utils/cron.ts
+++ backend/src/utils/cron.ts
@@ -3,7 +3,7 @@ import { PartRepository } from "../repositories/part.repository";
 
 const partRepository = new PartRepository();
 
-cron.schedule("0 3 * * *", async () => {
+cron.schedule("0 22 * * *", async () => {
   console.log("Executando atualização de preços de peças...");
   await partRepository.autoUpdatePrices();
   console.log("Finalizada atualização de preços de peças...");
